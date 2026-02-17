98e26a9f76e78d002e4864e938f2ab16e8533088
admin <admin@admins-MacBook-Pro.local>
2026-02-17 18:22:46 +0530

    added resume

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import coderImg from "../assets/right.svg";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiDownload, HiMail, HiSparkles } from "react-icons/hi";
-import resume from "../assets/Himanshi.pdf";
+import resume from "../assets/khoobchand.pdf";
 
 const Hero = () => {
   return (
